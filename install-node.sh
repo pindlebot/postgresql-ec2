1940d06e35463e9fb90dcbad7c4986f92a393fa2
Ben <menubar@protonmail.com>
2018-10-10 00:41:27 -0400

    new

--- install-node.sh
+++ install-node.sh
@@ -5,3 +5,11 @@ su ec2-user -c "source /home/ec2-user/.nvm/nvm.sh && nvm install node"
 su ec2-user -c "source /home/ec2-user/.nvm/nvm.sh && nvm use node"
 ln -s /home/ec2-user/.nvm/versions/node/v10.11.0/bin/node /usr/bin/node
 ln -s /home/ec2-user/.nvm/versions/node/v10.11.0/bin/npm /usr/bin/npm
+
+INSTANCE_ID=$(ec2-metadata -i | awk '{ print $2 }')
+SECURITY_GROUP=$(ec2-metadata -s | awk '{ print $2 }')
+
+aws ec2 authorize-security-group-ingress --group-name $SECURITY_GROUP --protocol tcp --port 80 --cidr '0.0.0.0/0'
+aws ec2 authorize-security-group-ingress --group-name $SECURITY_GROUP --protocol tcp --port 443 --cidr '0.0.0.0/0'
+mkdir -p /var/task
+curl -o /var/task/server.js https://gist.githubusercontent.com/unshift/5c14f58fb8e497a3a98663e4c5c6d7c7/raw/fcf30f7eaaeac8fda5916e8842baeae09c79cc9c/server.js
