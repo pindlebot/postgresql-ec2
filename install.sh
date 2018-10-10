c94cec18a1f8d8628cc49b68ea2731bc7e8a029f
Ben <menubar@protonmail.com>
2018-10-09 22:14:54 -0400

    new

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 echo SECRET_TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n1) > /tmp/secret
 
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-libs-10.5-1PGDG.rhel7.x86_64.rpm
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-10.5-1PGDG.rhel7.x86_64.rpm
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-contrib-10.5-1PGDG.rhel7.x86_64.rpm
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-devel-10.5-1PGDG.rhel7.x86_64.rpm
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-server-10.5-1PGDG.rhel7.x86_64.rpm
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-test-10.5-1PGDG.rhel7.x86_64.rpm
-# yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-pltcl-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-libs-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-contrib-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-devel-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-server-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-test-10.5-1PGDG.rhel7.x86_64.rpm
+yum install -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/postgresql10-pltcl-10.5-1PGDG.rhel7.x86_64.rpm
 
 /usr/pgsql-10/bin/postgresql-10-setup initdb
+systemctl enable postgresql-10
 
 curl -o /var/lib/pgsql/10/data/pg_hba.conf https://raw.githubusercontent.com/unshift/postgresql-ec2/master/pg_hba.conf
 sed -i 's|#listen_addresses = \x27localhost\x27|listen_addresses = \x27*\x27|' /var/lib/pgsql/10/data/postgresql.conf
@@ -18,7 +19,6 @@ sed -i 's|#port = 5432|port = 5432|' /var/lib/pgsql/10/data/postgresql.conf
 sed -i 's|#shared_preload_libraries = \x27\x27|shared_preload_libraries = \x27pg_cron\x27|' /var/lib/pgsql/10/data/postgresql.conf
 echo "cron.database_name = 'postgres'" >> /var/lib/pgsql/10/data/postgresql.conf
 
-systemctl enable postgresql-10
 systemctl start postgresql-10
 
 source /tmp/secret
